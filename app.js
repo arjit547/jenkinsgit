ed7b5f6ae8326aaff0333e82b639a1d608e3e3c5
arjit547 <97424724+arjit547@users.noreply.github.com>
2025-05-23 17:09:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Mumbai Indians\n');
+  res.end('Mumbai Indians IPL Winner\n');
 });
 
 server.listen(3000, () => {
